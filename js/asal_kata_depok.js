9b1d1ca4e870c7f819f027f86936c2c656421dbc
fathyaff <fathya.afifah@kokatto.com>
2018-09-25 22:34:45 +0700

    update asal kata depok

--- js/asal_kata_depok.js
+++ js/asal_kata_depok.js
@@ -1,48 +1,220 @@
-// Get the modal
-var modalPeta = document.getElementById('modalPeta');
-var modalPadepokan = document.getElementById('modalPadepokan');
-var modalAkronim = document.getElementById('modalAkronim');
+$(document).ready(function() {
+    // Get the modal
+    var modalPeta = document.getElementById('modalPeta');
+    var modalPadepokan = document.getElementById('modalPadepokan');
+    var modalAkronim = document.getElementById('modalAkronim');
 
-// Get the button that opens the modal
-var imgpeta = document.getElementById("peta");
-var imgpadepokan = document.getElementById("padepokan");
-var imgakronim = document.getElementById("akronim");
+    // Get the button that opens the modal
+    var imgpeta = document.getElementById("peta");
+    var imgpadepokan = document.getElementById("padepokan");
+    var imgakronim = document.getElementById("akronim");
 
-// Get the <span> element that closes the modal
-var span_close_peta = document.getElementsByClassName("close-modal-peta")[0];
-var span_close_padepokan = document.getElementsByClassName("close-modal-padepokan")[0];
-var span_close_akronim = document.getElementsByClassName("close-modal-akronim")[0];
+    // Get the <span> element that closes the modal
+    var span_close_peta = document.getElementsByClassName("close-modal-peta")[0];
+    var span_close_padepokan = document.getElementsByClassName("close-modal-padepokan")[0];
+    var span_close_akronim = document.getElementsByClassName("close-modal-akronim")[0];
 
-// When the user clicks on the button, open the modal
-imgpeta.onclick = function() {
-    modalPeta.style.display = "block";
-}
-imgpadepokan.onclick = function() {
-  modalPadepokan.style.display = "block";
-}
-imgakronim.onclick = function() {
-  modalAkronim.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span_close_peta.onclick = function() {
-    modalPeta.style.display = "none";
-}
-
-span_close_padepokan.onclick = function() {
-    modalPadepokan.style.display = "none";
-}
-
-span_close_akronim.onclick = function() {
-    modalAkronim.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    // When the user clicks on the button, open the modal
+    imgpeta.onclick = function() {
+        modalPeta.style.display = "block";
     }
-}
+    imgpadepokan.onclick = function() {
+    modalPadepokan.style.display = "block";
+    }
+    imgakronim.onclick = function() {
+    modalAkronim.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span_close_peta.onclick = function() {
+        modalPeta.style.display = "none";
+    }
+
+    span_close_padepokan.onclick = function() {
+        modalPadepokan.style.display = "none";
+    }
+
+    span_close_akronim.onclick = function() {
+        modalAkronim.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    /* ===============================================================
+    ==================== PLAY PAUSE PETA =============================
+    ==================================================================*/
+    var audioPetaTiti = document.getElementById("audioPetaTiti"); 
+    var audioPetaBondan = document.getElementById("audioPetaBondan"); 
+
+    function playAudioPetaTiti() { 
+        audioPetaTiti.play(); 
+    } 
+
+    function pauseAudioPetaTiti() { 
+        audioPetaTiti.pause(); 
+    } 
+
+    function playAudioPetaBondan() { 
+        audioPetaBondan.play(); 
+    } 
+
+    function pauseAudioPetaBondan() { 
+        audioPetaBondan.pause(); 
+    } 
+
+
+    $('#toggle-peta-titi').bind("click", function() {
+      if ($(this).attr("class") == "play-peta-titi"){
+        playAudioPetaTiti();
+        $(this).attr("class", "pause-peta-titi");
+      } else{
+        pauseAudioPetaTiti();
+        $(this).attr("class", "play-peta-titi");
+      }
+         
+    });
+
+    $('#toggle-peta-bondan').bind("click", function() {
+        if ($(this).attr("class") == "play-peta-bondan"){
+          playAudioPetaBondan();
+          $(this).attr("class", "pause-peta-bondan");
+        } else{
+          pauseAudioPetaBondan();
+          $(this).attr("class", "play-peta-bondan");
+        }
+      });
+
+    /* ===============================================================
+    ==================== PLAY PAUSE PADEPOKAN ========================
+    ==================================================================*/
+    var audioPadepokanTiti = document.getElementById("audioPadepokanTiti"); 
+    var audioPadepokanLili = document.getElementById("audioPadepokanLili"); 
+
+    function playAudioPadepokanTiti() { 
+        audioPadepokanTiti.play(); 
+    } 
+
+    function pauseAudioPadepokanTiti() { 
+        audioPadepokanTiti.pause(); 
+    } 
+
+    function playAudioPadepokanLili() { 
+        audioPadepokanLili.play(); 
+    } 
+
+    function pauseAudioPadepokanLili() { 
+        audioPadepokanLili.pause(); 
+    } 
+
+
+    $('#toggle-padepokan-titi').bind("click", function() {
+      if ($(this).attr("class") == "play-padepokan-titi"){
+        playAudioPadepokanTiti();
+        $(this).attr("class", "pause-padepokan-titi");
+      } else{
+        pauseAudioPadepokanTiti();
+        $(this).attr("class", "play-padepokan-titi");
+      }
+         
+    });
+
+    $('#toggle-padepokan-lili').bind("click", function() {
+        if ($(this).attr("class") == "play-padepokan-lili"){
+            playAudioPadepokanLili();
+          $(this).attr("class", "pause-padepokan-lili");
+        } else{
+            pauseAudioPadepokanLili();
+          $(this).attr("class", "play-padepokan-lili");
+        }
+      });
+    
+    /* ===============================================================
+    ==================== PLAY PAUSE AKRONIM ========================
+    ==================================================================*/
+    var audioAkronimYano = document.getElementById("audioAkronimYano"); 
+    var audioAkronimLili = document.getElementById("audioAkronimLili"); 
+    var audioAkronimFerdy = document.getElementById("audioAkronimFerdy"); 
+    var audioAkronimBoy = document.getElementById("audioAkronimBoy"); 
+
+    function playAudioAkronimYano() { 
+        audioAkronimYano.play(); 
+    } 
+
+    function pauseAudioAkronimYano() { 
+        audioAkronimYano.pause(); 
+    } 
+
+    function playAudioAkronimLili() { 
+        audioAkronimLili.play(); 
+    } 
+
+    function pauseAudioAkronimLili() { 
+        audioAkronimLili.pause(); 
+    } 
+
+    function playAudioAkronimFerdy() { 
+        audioAkronimFerdy.play(); 
+    } 
+
+    function pauseAudioAkronimFerdy() { 
+        audioAkronimFerdy.pause(); 
+    } 
+
+    function playAudioAkronimBoy() { 
+        audioAkronimBoy.play(); 
+    } 
+
+    function pauseAudioAkronimBoy() { 
+        audioAkronimBoy.pause(); 
+    } 
+
+    $('#toggle-akronim-yano').bind("click", function() {
+      if ($(this).attr("class") == "play-akronim-Yano"){
+        playAudioAkronimYano();
+        $(this).attr("class", "pause-akronim-yano");
+      } else{
+        pauseAudioAkronimYano();
+        $(this).attr("class", "play-akronim-yano");
+      }
+         
+    });
+
+    $('#toggle-akronim-ferdy').bind("click", function() {
+        if ($(this).attr("class") == "play-akronim-ferdy"){
+            playAudioAkronimFerdy();
+          $(this).attr("class", "pause-akronim-ferdy");
+        } else{
+            pauseAudioAkronimFerdy();
+          $(this).attr("class", "play-akronim-ferdy");
+        }
+      });
+
+      $('#toggle-akronim-lili').bind("click", function() {
+        if ($(this).attr("class") == "play-akronim-lili"){
+            playAudioAkronimLili();
+          $(this).attr("class", "pause-akronim-lili");
+        } else{
+            pauseAudioAkronimLili();
+          $(this).attr("class", "play-akronim-lili");
+        }
+      });
+
+      $('#toggle-akronim-boy').bind("click", function() {
+        if ($(this).attr("class") == "play-akronim-boy"){
+            playAudioAkronimBoy();
+          $(this).attr("class", "pause-akronim-boy");
+        } else{
+            pauseAudioAkronimBoy();
+          $(this).attr("class", "play-akronim-boy");
+        }
+      });
+  });
+
 
 
 
--- php/asal_kata_depok.php
+++ php/asal_kata_depok.php
@@ -21,6 +21,7 @@
             </div>
         </div>
         <div class="row asal-kata-depok-row">
+            <!-- <div class="col-6 col-md-3"></div> -->
             <div class="col-6 col-md-4">
                 <img id="peta" src="img/asets/asal_kata_depok/peta.png" alt="" width="30%" class=" img-responsive"/>
             </div>
@@ -30,6 +31,7 @@
             <div class="col-6 col-md-4">
                 <img id="akronim" src="img/asets/asal_kata_depok/akronim.png" alt="" width="30%" class=" img-responsive"/>
             </div>
+            <!-- <div class="col-6 col-md-2"></div> -->
         </div>
 </section>
         <!-- The Modal -->
@@ -39,36 +41,49 @@
                 <span class="close-modal-asal-kata-depok close-modal-peta">&times;</span>
                 <div class="row row-top">
                     <div class="col-8 col-md-8">
-                        <p class="modal-desc-content">“Depok itu memang sudah suatu wilayah sebelum datangnya Chastelein” begitu pernyataan Ibu Titi. 
-                        Pernyataan ini sejalan dengan adanya bukti berupa peta kadaster pada tahun 1696. 
-                        Tepat pada tanggal 18 Mei di tahun itu, tanah Depok dijual oleh seorang residen Cirebon bernama 
-                        Lucas van de Meur kepada Cornelis Chastelein dengan status kepemilikan nomor 872. 
-                        Sebelumnya, tanah itu diberikan kepada Lucas van de Meur pada tanggal 15 Oktober 1695. 
-                        Meskipun demikian, Mas Bondan menyatakan bahwa memang belum ada dokumen sejarah yang secara khusus 
-                        berisi informasi tentang asal mula nama Depok.
-                        </p>
-                    </div>
-                    <div class="col-4 col-md-4">
-                        <img src="img/asets/asal_kata_depok/gambar_peta.png" alt="" width="90%" class=" img-responsive"/>
-                    </div>
-                </div>
-                <div class="row">
-                    <!-- <div class="col-md-4 col-md-offset-6 carousel-box">
-                        <div class="carousel slide multi-item-carousel-asal-kata-depok" id="theCarousel">
-                            <div class="carousel-inner">
-                            <div class="item active">
-                                <div class="col-xs-4"><img id="titi" src="img/asets/asal_kata_depok/bu_titi.png" class="img-responsive"></a></div>
-                            </div>
-                            <div class="item">
-                                <div class="col-xs-4"><img id="bondan" src="img/asets/asal_kata_depok/mas_bondan.png" class="img-responsive"></a></div>
-                            </div>
-                             Example item end
-                            </div>
-                            <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-                            <a class="right carousel-control" href="#theCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+                        <div class="row">
+                            <p class="modal-desc-content">“Depok itu memang sudah suatu wilayah sebelum datangnya Chastelein” begitu pernyataan Ibu Titi. 
+                            Pernyataan ini sejalan dengan adanya bukti berupa peta kadaster pada tahun 1696. 
+                            Tepat pada tanggal 18 Mei di tahun itu, tanah Depok dijual oleh seorang residen Cirebon bernama 
+                            Lucas van de Meur kepada Cornelis Chastelein dengan status kepemilikan nomor 872. 
+                            Sebelumnya, tanah itu diberikan kepada Lucas van de Meur pada tanggal 15 Oktober 1695. 
+                            Meskipun demikian, Mas Bondan menyatakan bahwa memang belum ada dokumen sejarah yang secara khusus 
+                            berisi informasi tentang asal mula nama Depok.
+                            </p>
                         </div>
-                    </div> -->
+                        <div class="row">
+                            <div class="col-6 col-md-4">
+                                <audio id="audioPetaBondan">
+                                    <source src="audio/proklamasi.mp3" type="audio/mpeg">
+                                </audio>
+                                <div style="position:relative" id="toggle-peta-bondan" class="play-peta-bondan"></div>
+                                <div style="position:absolute; left:0; bottom:0; float:right;margin-left: 45%;margin-bottom: -3%;">
+                                    <p>Mas Bondan</p>
+                                </div>
+                            </div>
+                            <div class="col-6 col-md-4">
+                                <audio id="audioPetaTiti">
+                                    <source src="audio/proklamasi.mp3" type="audio/mpeg">
+                                </audio>
+                                <div style="position:relative" id="toggle-peta-titi" class="play-peta-titi"></div>
+                                <div style="position:absolute; left:0; bottom:0;float:left;margin-left:23%;margin-bottom:-3%">
+                                    <p>Bu Titi</p>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="col-4 col-md-4 portfolio-hover">
+                        <img id="petaimg" src="img/asets/asal_kata_depok/gambar_peta.png" alt="" width="90%" class=" img-responsive"/>
+                        <div class="portfolio-hover-content" style="position:absolute; top:0">
+                            <h2>Tahukah kamu?</h2>
+                            <p style="text-align:justify">Peta kadaster adalah jenis peta dengan skala yang sangat besar. 
+                            Pada umumnya peta ini digunakan untuk menggambarkan peta kepemilikan tanah atau perencanaan bangunan.</p>
+                        </div>
+
+                    </div>
                 </div>
+                
             </div>
         </div>
         <!-- The Modal -->
@@ -78,34 +93,46 @@
                 <span class="close-modal-asal-kata-depok close-modal-padepokan">&times;</span>
                 <div class="row row-top">
                     <div class="col-8 col-md-8">
-                        <p class="modal-desc-content">Nama Depok nyatanya tak hanya ditemukan sebagai nama wilayah di bagian selatan Batavia.
-                             Di wilayah lain seperti Yogyakarta dan Semarang juga ditemukan nama Depok. 
-                             Menurut Meneer Lilie, wilayah Depok berfungsi sebagai tempat untuk memperoleh ilmu pada zaman 
-                             kerajaan-kerajaan kuno di Nusantara. Namun demikian, belum ada bukti arkeologis yang dapat memperkuat dugaan ini. 
-                             Bagaimana menurutmu?
-                        </p>
-                    </div>
-                    <div class="col-4 col-md-4">
-                        <img src="img/asets/asal_kata_depok/gambar_padepokan.png" alt="" width="90%" class=" img-responsive"/>
-                    </div>
-                </div>
-                <div class="row">
-                    <!-- <div class="col-md-4 col-md-offset-6 carousel-box">
-                        <div class="carousel slide multi-item-carousel-asal-kata-depok" id="theCarousel">
-                            <div class="carousel-inner">
-                            <div class="item active">
-                                <div class="col-xs-4"><img id="titi" src="img/asets/asal_kata_depok/bu_titi.png" class="img-responsive"></a></div>
-                            </div>
-                            <div class="item">
-                                <div class="col-xs-4"><img id="bondan" src="img/asets/asal_kata_depok/mas_bondan.png" class="img-responsive"></a></div>
-                            </div>
-                             Example item end
-                            </div>
-                            <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-                            <a class="right carousel-control" href="#theCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+                        <div class="row">
+                            <p class="modal-desc-content">Nama Depok nyatanya tak hanya ditemukan sebagai nama wilayah di bagian selatan Batavia.
+                                Di wilayah lain seperti Yogyakarta dan Semarang juga ditemukan nama Depok. 
+                                Menurut Meneer Lilie, wilayah Depok berfungsi sebagai tempat untuk memperoleh ilmu pada zaman 
+                                kerajaan-kerajaan kuno di Nusantara. Namun demikian, belum ada bukti arkeologis yang dapat memperkuat dugaan ini. 
+                                Bagaimana menurutmu?
+                            </p>
                         </div>
-                    </div> -->
+                        <div class="row">
+                            <div class="col-6 col-md-4">
+                                <audio id="audioPadepokanTiti">
+                                    <source src="audio/proklamasi.mp3" type="audio/mpeg">
+                                </audio>
+                                <div style="position:relative" id="toggle-padepokan-titi" class="play-padepokan-titi"></div>
+                                <div style="position:absolute; left:0; bottom:0; float:right;margin-left: 56%;margin-bottom: -3%;">
+                                    <p>Bu Titi</p>
+                                </div>
+                            </div>
+                            <div class="col-6 col-md-4">
+                                <audio id="audioPadepokanLili">
+                                    <source src="audio/proklamasi.mp3" type="audio/mpeg">
+                                </audio>
+                                <div style="position:relative" id="toggle-padepokan-lili" class="play-padepokan-lili"></div>
+                                <div style="position:absolute; left:0; bottom:0;float:left;margin-left:15%;margin-bottom:-3%">
+                                    <p>Meneer Lilie</p>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="col-4 col-md-4 portfolio-hover">
+                        <img src="img/asets/asal_kata_depok/gambar_padepokan.png" alt="" width="90%" class=" img-responsive"/>
+                        <div class="portfolio-hover-content" style="position:absolute; top:0">
+                            <h2>Tahukah kamu?</h2>
+                                <p style="text-align:justify">dalam kamus bahasa Jawa, diketahui bahwa depok berasal dari kata
+                                    "padepokan" yang berarti tempat berguru</p>
+                            </div>
+                    </div>
                 </div>
+                
             </div>
         </div>
          <!-- The Modal -->
@@ -115,36 +142,63 @@
                 <span class="close-modal-asal-kata-depok close-modal-akronim">&times;</span>
                 <div class="row row-top">
                     <div class="col-8 col-md-8">
-                        <p class="modal-desc-content">
-                        Banyak pula yang berpendapat bahwa Depok merupakan akronim dari "De Eerste Protestanse Organisatie van Kristenen". 
-                        Istilah ini dibuat dari bahasa Belanda yang berarti organisasi kristen protestan pertama. 
-                        Tentu saja organisasi yang dimaksud merujuk pada komunitas kristen yang dibentuk di wilayah Depok pada 
-                        masa Cornelis Chastelein. Namun demikian, istilah tersebut bukan merupakan akronim Depok yang sebenarnya. 
-                        Menurut Para Penutur, istilah itu dibentuk oleh komunitas orang-orang Depok yang hijrah ke Belanda setelah 
-                        peristiwa Gedoran Depok. Istilah lain yang juga dibuat dengan akronim Depok yaitu “Daerah Pemukiman Orang Kota” 
-                        seiring dengan dibangunnya Perumnas Depok sebagai tempat pemukiman orang-orang yang bekerja di ibukota.
-                        </p>
-                    </div>
-                    <div class="col-4 col-md-4">
-                        <img src="img/asets/asal_kata_depok/DeEerste.png" alt="" width="90%" class=" img-responsive"/>
-                    </div>
-                </div>
-                <div class="row">
-                    <!-- <div class="col-md-4 col-md-offset-6 carousel-box">
-                        <div class="carousel slide multi-item-carousel-asal-kata-depok" id="theCarousel">
-                            <div class="carousel-inner">
-                            <div class="item active">
-                                <div class="col-xs-4"><img id="titi" src="img/asets/asal_kata_depok/bu_titi.png" class="img-responsive"></a></div>
-                            </div>
-                            <div class="item">
-                                <div class="col-xs-4"><img id="bondan" src="img/asets/asal_kata_depok/mas_bondan.png" class="img-responsive"></a></div>
-                            </div>
-                             Example item end
-                            </div>
-                            <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-                            <a class="right carousel-control" href="#theCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+                        <div class="row">
+                            <p class="modal-desc-content">
+                            Banyak pula yang berpendapat bahwa Depok merupakan akronim dari "De Eerste Protestanse Organisatie van Kristenen". 
+                            Istilah ini dibuat dari bahasa Belanda yang berarti organisasi kristen protestan pertama. 
+                            Tentu saja organisasi yang dimaksud merujuk pada komunitas kristen yang dibentuk di wilayah Depok pada 
+                            masa Cornelis Chastelein. Namun demikian, istilah tersebut bukan merupakan akronim Depok yang sebenarnya. 
+                            Menurut Para Penutur, istilah itu dibentuk oleh komunitas orang-orang Depok yang hijrah ke Belanda setelah 
+                            peristiwa Gedoran Depok. Istilah lain yang juga dibuat dengan akronim Depok yaitu “Daerah Pemukiman Orang Kota” 
+                            seiring dengan dibangunnya Perumnas Depok sebagai tempat pemukiman orang-orang yang bekerja di ibukota.
+                            </p>
                         </div>
-                    </div> -->
+                        <div class="row">
+                            <div class="col-6 col-md-3">
+                                <audio id="audioAkronimYano">
+                                    <source src="audio/proklamasi.mp3" type="audio/mpeg">
+                                </audio>
+                                <div style="position:relative" id="toggle-akronim-yano" class="play-akronim-yano"></div>
+                                <div style="position:absolute; left:0; bottom:0; float:right;margin-left: 25%;margin-bottom: -3%;">
+                                    <p>Om Yano</p>
+                                </div>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <audio id="audioAkronimLili">
+                                        <source src="audio/proklamasi.mp3" type="audio/mpeg">
+                                </audio>
+                                <div style="position:relative" id="toggle-akronim-lili" class="play-akronim-lili"></div>
+                                <div style="position:absolute; left:0; bottom:0; float:right;margin-left: 20%;margin-bottom: -3%;">
+                                    <p>Meneer Lilie</p>
+                                </div>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <audio id="audioAkronimFerdy">
+                                        <source src="audio/proklamasi.mp3" type="audio/mpeg">
+                                </audio>
+                                <div style="position:relative" id="toggle-akronim-ferdy" class="play-akronim-ferdy"></div>
+                                <div style="position:absolute; left:0; bottom:0; float:right;margin-left: 23%;margin-bottom: -3%;">
+                                    <p>Om Ferdy</p>
+                                </div>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <audio id="audioAkronimBoy">
+                                        <source src="audio/proklamasi.mp3" type="audio/mpeg">
+                                </audio>
+                                <div style="position:relative" id="toggle-akronim-boy" class="play-akronim-boy"></div>
+                                <div style="position:absolute; left:0; bottom:0; float:right;margin-left: 27%;margin-bottom: -3%;">
+                                    <p>Om Boy</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4 col-md-4 portfolio-hover">
+                        <img id="deEersteimg" src="img/asets/asal_kata_depok/DeEerste.png" alt="" width="90%" class=" img-responsive"/>
+                        <div class="portfolio-hover-content" style="position:absolute; top:0">
+                            <p style="text-align:justify">Akronim ini merupakan bentuk kerinduan warga Depok di Belanda
+                            akan tanah kelahirannya, sekaligus pengingat bahwa komunitas kristen protestan pertama dibentuk di Depok.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
